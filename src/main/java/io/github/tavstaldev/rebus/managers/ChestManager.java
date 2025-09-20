c6fe6c8da1d664c805706ad6785f896fc4df932a
tavstal <tavstal@gmail.com>
2025-09-20 18:52:27 +0200

    fix: initialize chests set to a new HashSet in load method

--- src/main/java/io/github/tavstaldev/rebus/managers/ChestManager.java
+++ src/main/java/io/github/tavstaldev/rebus/managers/ChestManager.java
@@ -44,7 +44,7 @@ public class ChestManager {
     public final Set<Location> chestsUnderUnlocking = new HashSet<>();
 
     public void load() {
-        chests = (chests == null) ? new HashSet<>() : new HashSet<>(chests);
+        chests = new HashSet<>();
 
         // Ensure chests directory exists and create default chest files if necessary
         var chestsDir = Paths.get(Rebus.Instance.getDataFolder().getPath(), "chests").toFile();
