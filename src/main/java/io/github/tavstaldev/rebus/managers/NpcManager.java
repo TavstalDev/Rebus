2d5dee0742f31c4ab30f18582eff53a2b9fdb90c
Tavstal <tavstal@gmail.com>
2026-07-01 03:07:51 +0200

    fix: Refactor skin trait usage

--- src/main/java/io/github/tavstaldev/rebus/managers/NpcManager.java
+++ src/main/java/io/github/tavstaldev/rebus/managers/NpcManager.java
@@ -3,6 +3,7 @@ package io.github.tavstaldev.rebus.managers;
 import io.github.tavstaldev.rebus.Rebus;
 import io.github.tavstaldev.rebus.RebusConfig;
 import io.github.tavstaldev.rebus.models.NpcTrait;
+import io.github.tavstaldev.yggra.core.logger.YggraLogger;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.npc.NPCRegistry;
@@ -15,14 +16,16 @@ import org.bukkit.entity.Player;
  * Manages the creation, removal, and identification of NPCs in the Rebus plugin.
  */
 public class NpcManager {
+    private final YggraLogger logger;
     private final Rebus plugin;
     private final NPCRegistry registry;
 
     /**
      * Initializes the NpcManager with the Rebus plugin instance and NPC registry.
      */
-    public NpcManager() {
-        this.plugin = Rebus.Instance;
+    public NpcManager(Rebus plugin) {
+        this.plugin = plugin;
+        this.logger = plugin.logger().withModule(NpcManager.class);
         this.registry = CitizensAPI.getNPCRegistry();
     }
 
@@ -33,39 +36,40 @@ public class NpcManager {
      */
     public void spawnNPC(Player player) {
         Location location = player.getLocation();
-        RebusConfig config = Rebus.config();
+        RebusConfig config = plugin.config();
 
         String npcName = config.npcName;
         NPC npc = this.registry.createNPC(EntityType.PLAYER, npcName);
-        npc.spawn(location);
         String skinData = config.npcSkin;
         if (!skinData.isEmpty()) {
             try {
-                npc.getOrAddTrait(SkinTrait.class).setSkinPersistent(npcName, "", skinData);
+                npc.getOrAddTrait(SkinTrait.class).setSkinPersistent(npcName, config.npcSignature, skinData);
             }
             catch (Exception e) {
-                Rebus.logger().warn("Failed to apply skin to NPC: " + e.getMessage());
+                logger.warn("Failed to apply skin to NPC.", e);
             }
         }
+        npc.spawn(location);
         npc.setProtected(true);
         try {
-            npc.addTrait(new NpcTrait());
-            Rebus.logger().info("Rebus NPC created for player: " + player.getName() + " (NPC ID: " + npc.getId() + ")");
+            var trait = new NpcTrait();
+            npc.addTrait(trait);
+            logger.info("Rebus NPC created for player: " + player.getName() + " (NPC ID: " + npc.getId() + ")");
         }
         catch (Exception e) {
-            Rebus.logger().warn("Error setting NPC persistent data: " + e.getMessage());
+            logger.warn("Error setting NPC persistent data.", e);
         }
-        plugin.sendLocalizedMsg(player, "NPC.Spawned");
+        plugin.chat().sendLocalizedMsg(player, "npc.spawned");
     }
 
     /**
      * Removes all NPCs created by the Rebus plugin.
      */
-    public void removeAllNpcs() {
+    public void removeAllNPCs() {
         for (NPC npc : registry.sorted()) {
             if (isRebusNPC(npc)) {
                 npc.destroy();
-                Rebus.logger().info("Removed Rebus NPC (ID: " + npc.getId() + ")");
+                logger.info("Removed Rebus NPC (ID: " + npc.getId() + ")");
             }
         }
     }
@@ -77,9 +81,8 @@ public class NpcManager {
      * @return True if the NPC is a Rebus NPC, false otherwise.
      */
     public boolean isRebusNPC(NPC npc) {
-        if (npc == null) {
+        if (npc == null)
             return false;
-        }
         return npc.hasTrait(NpcTrait.class);
     }
 }
